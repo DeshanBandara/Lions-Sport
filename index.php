6ab6413049acdcd3f2c3a4487d1a53d88b44882e
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-09 22:51:01 +0530

    add footer and navbar

--- index.php
+++ index.php
@@ -9,33 +9,44 @@
 <body>
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">My Website</a>
+        <a class="navbar-brand br" href="#">Lions Sports</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Lions Sports</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Signin</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 
+    <div>
+        <h3> welcome</h3>
+        <h1>lions sports</h1>
+    </div>
+
+    <div>
+        <footer class="footer fixed-bottom bg-dark text-light text-center py-3">
+            <div class="container">
+                &copy; 2023 Lions Sports. All rights reserved.
+            </div>
+        </footer>
+    </div>
     <!-- Bootstrap JS and jQuery scripts (required for dropdown functionality) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-    </div>
 </body>
 </html>
