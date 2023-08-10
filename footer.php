8fde03dfbe584a75bf5754a3423d4a0ac05f8119
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-10 09:31:08 +0530

    add footer.php file

---
+++ footer.php
@@ -0,0 +1,14 @@
+    <div>
+        <footer class="footer fixed-bottom bg-dark text-light text-center py-3">
+            <div class="container">
+                &copy; 2023 Lions Sports. All rights reserved.
+            </div>
+        </footer>
+    </div>
+    
+    <!-- Bootstrap JS and jQuery scripts (required for dropdown functionality) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
