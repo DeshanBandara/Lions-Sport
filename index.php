4d20a49e6430d95558210661fb09502ada2e8975
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-10 09:31:28 +0530

    add footer and navbar php file

--- index.php
+++ index.php
@@ -1,53 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./resources/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles.css">
-    <title>Lions Sports</title>
-</head>
-<body>
-    <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand br" href="#">Lions Sports</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
+<?php include('./navbar.php') ?>
 
     <div>
         <h3> welcome</h3>
         <h1>lions sports</h1>
     </div>
 
-    <div>
-        <footer class="footer fixed-bottom bg-dark text-light text-center py-3">
-            <div class="container">
-                &copy; 2023 Lions Sports. All rights reserved.
-            </div>
-        </footer>
-    </div>
-    <!-- Bootstrap JS and jQuery scripts (required for dropdown functionality) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
