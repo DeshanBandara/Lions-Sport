bb56603d310202dca286e1dd5f8061a20348e4af
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-10 09:16:59 +0530

    added image files

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
     <title>Lions Sports</title>
 </head>
 <body>
