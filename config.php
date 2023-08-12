c959422385e66ab7f0610622a84f3db77ce47d89
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-13 00:29:23 +0530

    added config file and connected it into the database

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'lions-sports';
+
+    $connect=mysqli_connect('localhost', 'root', '', 'lions-sports') or die("Data Base Connection Faild!, Please check your sql files");
+
+?>
